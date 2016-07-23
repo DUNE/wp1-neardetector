c9b3c46590322dd6ddba3a0b54b39b757ab2b3b2
Justo Martin-Albo <jmalbos@users.noreply.github.com>
2016-07-23 19:09:27 +0100

    Updates in BeamSpillGenerator.

--- gastpc/G4Sim/BeamSpillGenerator.cxx
+++ gastpc/G4Sim/BeamSpillGenerator.cxx
@@ -28,7 +28,11 @@ BeamSpillGenerator::~BeamSpillGenerator()
 
 void BeamSpillGenerator::AddGHepSource(G4String path, G4double mean)
 {
-  GHepReader* ghep_source = new GHepReader(path,mean);
+  G4cerr << "BeamSpillGenerator::AddGHepSource" << G4endl;
+  G4cerr << "Path: " << path << G4endl;
+  G4cerr << "Mean: " << mean << G4endl;
+
+  GHepReader* ghep_source = new GHepReader(path, mean);
   sources_.push_back(ghep_source);
 }
 
